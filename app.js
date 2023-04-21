3ceef80070123b7a40e9da5c17bf668b43da583e
Srinivas Thiru <sri@Sris-Mac.local>
2023-04-21 15:07:25 -0400

    Level 1 Protection

---
+++ app.js
@@ -0,0 +1,92 @@
+//jshint esversion:6
+
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+// const secret = "thisisorusecret";
+// userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/logout", (req, res) => {
+    res.render("home");
+});
+
+//app.route("/login").get().post();
+
+
+
+app.route("/login")
+
+.get( (req, res) => {
+    res.render("login");
+})
+
+.post((req, res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email : username})
+    .then(user => {
+        if (user.password === password){
+            res.render("secrets");
+        }
+    });
+});
+
+
+
+
+app.route("/register")
+
+.get((req, res) => {
+    res.render("register");
+})
+
+.post((req, res) =>{
+    const newUser = new User({
+        email : req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+    .then(result => {
+        console.log(result);
+        res.render("secrets");
+    })
+    .catch(err => {
+        console.log("Error: "+ err)
+    });
+});
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server started successfully at :3000");
+});
+
+
